1a98852eda50f940fe735dbbb4e9f20eaf1b91a7
Guidantas28 <73999334+Guidantas28@users.noreply.github.com>
2021-04-18 15:29:31 -0300

    Add files via upload

---
+++ gerador-senhas/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path'); // CommonJS
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  esmodules: true,
+                },
+              },
+            ],
+          ],
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  devtool: 'source-map'
+};
